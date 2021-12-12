d7ddf2bcf80ec284755fb725e5c0dce81e488996
EC2 Default User <naoya.take10.01@gmail.com>
2021-12-12 01:18:23 +0000

    search

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  get '/search' => 'searchs#search'
+
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update] do
     resource :relationships,only:[:create,:destroy]
